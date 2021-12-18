852d843688d1341b6da78197dfcb9b7bc6d08cca
Rafael Ángel <rafaelmilanes91@gmail.com>
2021-12-18 17:45:17 +0000

    Created Razor Pages Project

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
-using System;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace dotnetcore
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
